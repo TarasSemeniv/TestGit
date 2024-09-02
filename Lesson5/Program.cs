0d8009698269b7d1507bb71e0115d5336960053e
Тарас Семенів <tarassemeniv2@gmail.com>
2024-09-02 19:50:47 +0200

    Create class Person

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,3 +1,39 @@
-﻿Console.Write("Enter name: ");
-string name = Console.ReadLine();
-Console.WriteLine("Hello, " + name);
+﻿
+class Program
+{
+    static int Main(string[] args)
+    {
+        Person prs = new Person("Taras", "Semeniv", "0001", 18, 1000);
+        prs.withdraw(500);
+        prs.show_info();
+        return 0;
+    }
+}
+class Person
+{
+    string name, surname, ID;
+    int age, balance;
+    public Person(string name, string surname, string ID, int age, int balance)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.ID = ID;
+        this.age = age;
+        this.balance = balance;
+    }
+    public void withdraw(int value)
+    {
+        if (value >= balance)
+        {
+            Console.WriteLine("The value canot be greater than the balance");
+        }
+        else
+        {
+
+        }
+    }
+    public void show_info()
+    {
+        Console.WriteLine($"Name: {name}\nSurname: {surname}\nID: {ID}\nAge: {age}\nBalance: {balance}");
+    }
+}
